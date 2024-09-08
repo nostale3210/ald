8207e3db8b6ede0519fd801edbbace5a228acffe
feiluuyre <fei@timesink>
2024-09-09 00:56:17 +0200

    add janky bootloader update for fedora grubx64
    add example

--- files/ald-config.sh
+++ files/ald-config.sh
@@ -1,6 +1,10 @@
-#SOURCE_IMAGE=
-#LOCAL_TAG=
-#ALD_PATH=
-#BOOT_PATH=
-#STATE=
-#KEEP_DEPS=
+# This file is sourced by ALD to determine the configuration.
+# To change the default, uncomment a line and change the assigned value.
+
+# SOURCE_IMAGE=ghcr.io/nostale3210/timesinkc-main:latest
+# LOCAL_TAG=localhost/ald-root
+# ALD_PATH=/var/deployments
+# BOOT_PATH=/boot
+# EFI_PATH="$BOOT_PATH/efi"
+# STATE=keep
+# KEEP_DEPS=4
