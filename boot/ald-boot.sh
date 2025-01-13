7e037fd6d9d9cda71aa93489863ca825474643db
nostale3210 <52334804+nostale3210@users.noreply.github.com>
2025-01-13 16:45:46 +0100

    improve logging in ald-boot.sh
    (and make this thing actually boot)

--- boot/ald-boot.sh
+++ boot/ald-boot.sh
@@ -4,22 +4,26 @@ type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
 
 
 ald_boot=$(getarg ald.boot)
+echo "Cmdline: $ald_boot"
 [ -z "$ald_boot" ] && exit 0
 
 active_boot=$(cat "/sysroot/.ald/current" 2>/dev/null)
+echo "Current: $active_boot"
 [ -z "$active_boot" ] && exit 1
 
 
 [ "$active_boot" = "$ald_boot" ] && \
-    echo "Current: $active_boot" && \
     echo "Cmdline: $ald_boot" && \
-    (mount -o bind,ro "/sysroot/usr" "/sysroot/usr" || printf "Couldn't bind mount /usr") && \
-    (mount -o bind,rw "/sysroot/usr/local" "/sysroot/usr/local" || printf "Couldn't bind mount /usr/local") && \
+    echo "Current: $active_boot" && \
+    (mount -o bind,ro "/sysroot/usr" "/sysroot/usr" || printf "Couldn't bind mount /usr\n") && \
+    (mount -o bind,rw "/sysroot/usr/local" "/sysroot/usr/local" || printf "Couldn't bind mount /usr/local\n") && \
     (chattr +i "/sysroot/" || printf "Couldn't lock /.\n") && \
-    mountpoint "/sysroot/.ald" || mount -o bind,ro "/sysroot/.ald" "/sysroot/.ald"
-    exit 0
+    (mountpoint "/sysroot/.ald" || mount -o bind,ro "/sysroot/.ald" "/sysroot/.ald") && exit 0
 
 mount -o remount,rw /sysroot
 [ ! -d /sysroot/usr ] && mkdir -p /sysroot/usr
+
+echo "Cmdline: $ald_boot (next)"
+echo "Current: $active_boot"
 
 ald swap "$ald_boot" --initramfs
