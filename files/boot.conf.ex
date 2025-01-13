bbe205bfdd97b87bef4505cf0df9a6aa2a4e0edc
feiluuyre <fei@timesink>
2025-01-13 08:29:57 +0100

    ensure ald is executable

--- files/boot.conf.ex
+++ files/boot.conf.ex
@@ -2,7 +2,7 @@ title ALD Deployment INSERT_DEPLOYMENT
 version INSERT_DEPLOYMENT
 linux /INSERT_DEPLOYMENT/vmlinuz
 initrd /INSERT_DEPLOYMENT/initramfs.img
-options root=UUID=INSERT_ROOT_UUID_HERE ro rd.luks.uuid=luks-INSERT_LUKS_UUID_HERE rhgb quiet init=ALD_PATH/init/INSERT_DEPLOYMENT.sh rd.luks.options=tpm2-device=auto,discard
+options root=UUID=INSERT_ROOT_UUID_HERE ro rd.luks.uuid=luks-INSERT_LUKS_UUID_HERE rhgb quiet ald.boot=INSERT_DEPLOYMENT rd.luks.options=tpm2-device=auto,discard
 grub_users $grub_users
 grub_arg --unrestricted
 grub_class fedora
