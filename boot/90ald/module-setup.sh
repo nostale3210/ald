9706a7254cef18e8835858bce7a8784f748bc55b
feiluuyre <fei@timesink>
2025-01-27 08:01:55 +0100

    cleanup dracut module

--- boot/90ald/module-setup.sh
+++ boot/90ald/module-setup.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-installkernel() {
-    return 0
-}
-
 check() {
-    if [[ -x $systemdutildir/systemd ]] && [[ -x /usr/libexec/ald-boot.sh ]]; then
+    if [[ -x "$systemdutildir"/systemd ]] && [[ -x /usr/libexec/ald-boot.sh ]]; then
         return 255
     fi
 
@@ -13,24 +9,21 @@ check() {
 }
 
 depends() {
+    echo bash systemd
+
     return 0
 }
 
 install() {
-    dracut_install /usr/libexec/ald-boot.sh
-    dracut_install /usr/bin/ald
-    dracut_install /usr/lib/ald/bootloader.sh
-    dracut_install /usr/lib/ald/container.sh
-    dracut_install /usr/lib/ald/create.sh
-    dracut_install /usr/lib/ald/info.sh
-    dracut_install /usr/lib/ald/space.sh
-    dracut_install /usr/lib/ald/swap.sh
-    dracut_install /usr/lib/ald/utils.sh
+    
+    inst_dir /usr/lib/ald
 
-    inst_simple "${systemdsystemunitdir}/ald-boot.service"
-    mkdir -p "${initdir}${systemdsystemconfdir}/initrd-root-fs.target.wants"
-    ln_r "${systemdsystemunitdir}/ald-boot.service" \
-        "${systemdsystemconfdir}/initrd-root-fs.target.wants/ald-boot.service"
+    inst_multiple /usr/lib/ald/*.sh \
+        /usr/libexec/ald-boot.sh \
+        "$systemdsystemunitdir"/ald-boot.service \
+        ald
+    
+    $SYSTEMCTL -q --root "$initdir" enable ald-boot.service
 
     # Dependencies
 }
