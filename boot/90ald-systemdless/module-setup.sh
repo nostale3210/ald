3c0559e79821198ecc61c3864c76e49df6ffa905
fei <fei@timesink>
2025-02-20 03:04:31 +0100

    add void container for testing systemdless environments

---
+++ boot/90ald-systemdless/module-setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+check() {
+    if [[ -x /usr/libexec/ald-boot.sh ]]; then
+        return 255
+    fi
+
+    return 1
+}
+
+depends() {
+    echo bash
+
+    return 0
+}
+
+install() {
+    
+    inst_dir /usr/lib/ald
+
+    inst_multiple /usr/lib/ald/*.sh \
+        /usr/libexec/ald-boot.sh \
+        ald
+
+    inst_hook pre-pivot 40 "$moddir/ald-hook.sh"
+    
+    # Dependencies
+}
